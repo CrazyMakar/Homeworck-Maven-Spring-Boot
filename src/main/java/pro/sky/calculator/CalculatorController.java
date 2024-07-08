cc4b6a78bcd309bda9b29d7831964fa109b5af19
Sergey Makarov <nucler-makar@rambler.ru>
2024-07-08 20:50:13 +0300

    Готовое ДЗ с исправлением

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
+    private static final String mess = "один (оба) из параметров не определён(ы)";
     private final CalculatorService calculatorService;
 
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
+    }
+
+    public Boolean verifiValue(Integer num1, Integer num2) {
+        return (num1 == null || num2 == null);
     }
 
     @GetMapping
@@ -21,26 +26,44 @@ public class CalculatorController {
     }
 
     @GetMapping(path = "/plus")
-    public String plus(@RequestParam int num1, @RequestParam int num2) {
-        var result = calculatorService.plus(num1, num2);
-        return num1 + " + " + num2 + " = " + result;
+    public String plus(@RequestParam(required = false) Integer num1,
+                       @RequestParam(required = false) Integer num2) {
+        if (verifiValue(num1, num2) == true) {
+            return mess;
+        } else {
+            var result = calculatorService.plus(num1, num2);
+            return num1 + " + " + num2 + " = " + result;
+        }
     }
 
-
     @GetMapping(path = "/minus")
-    public String minus(@RequestParam int num1, @RequestParam int num2) {
-        var result = calculatorService.minus(num1, num2);
-        return num1 + " - " + num2 + " = " + result;
+    public String minus(@RequestParam(required = false) Integer num1,
+                        @RequestParam(required = false) Integer num2) {
+        if (verifiValue(num1, num2) == true) {
+            return mess;
+        } else {
+            var result = calculatorService.minus(num1, num2);
+            return num1 + " - " + num2 + " = " + result;
+        }
     }
 
     @GetMapping(path = "/multiply")
-    public String multiply(@RequestParam int num1, @RequestParam int num2) {
-        var result = calculatorService.multiply(num1, num2);
-        return num1 + " * " + num2 + " = " + result;
+    public String multiply(@RequestParam(required = false) Integer num1,
+                           @RequestParam(required = false) Integer num2) {
+        if (verifiValue(num1, num2) == true) {
+            return mess;
+        } else {
+            var result = calculatorService.multiply(num1, num2);
+            return num1 + " * " + num2 + " = " + result;
+        }
     }
 
     @GetMapping(path = "/divide")
-    public String divide(@RequestParam int num1, @RequestParam int num2) {
+    public String divide(@RequestParam(required = false) Integer num1,
+                         @RequestParam(required = false) Integer num2) {
+        if (verifiValue(num1, num2) == true) {
+            return mess;
+        }
         if (num2 == 0) {
             return "Делить на ноль нельзя!";
         }
